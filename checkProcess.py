1fa2e07aa360e6752df6b65bd0328626b6ad42dc
instinctd <47389093+instinctd@users.noreply.github.com>
2020-10-13 10:46:49 +0300

    Update checkProcess.py

--- checkProcess.py
+++ checkProcess.py
@@ -1,5 +1,9 @@
+from psutil import process_iter
+from subprocess import check_output
 import psutil
-import socket
+
+def getPid(name):
+    return int(check_output(["pidof","-s",name]))
 
 def checkIfProcessRunning(processName):
     for proc in psutil.process_iter():
@@ -9,19 +13,17 @@ def checkIfProcessRunning(processName):
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     return False;
-    
-if checkIfProcessRunning('Firefox'):
-    print('OK: nginx is running')
+
+if checkIfProcessRunning('nginx'):
+    print("OK: nginx is running")
 else:
-    print('Warning: nginx is down')
+    print("WARNING: nginx is not running")
+    
+r = getPid("nginx")
 
-def isOpen(ip,port):
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-      s.connect((ip, int(port)))
-      s.shutdown(2)
-      return "Connetion to 80 port ESTABLISHED"
-   except:
-      return "Port closed"
-
-print(isOpen('127.0.0.1',80))
+for proc in process_iter():
+    for conns in proc.connections(kind='inet'):
+        if conns.laddr.pid == r and  conns.laddr.port == 80:
+            print("OK: nginx is running on 80 port")
+        if conns.laddr.pid == r and conns.laddr.port != 80:
+            print("WARNING: nginx not at 80's port")
